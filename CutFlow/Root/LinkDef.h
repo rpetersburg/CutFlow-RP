5c5b65b9a839e04abe390d03251f53f4f96bb170
rpetersburg <rpetersburg@gmail.com>
2014-03-26 15:20:41 +0100

    update:

--- CutFlow/Root/LinkDef.h
+++ CutFlow/Root/LinkDef.h
@@ -3,7 +3,6 @@
 
 #include <CutFlow/HiggsAnalysis.h>
 
-
 #ifdef __CINT__
 
 #pragma link off all globals;
@@ -11,7 +10,12 @@
 #pragma link off all functions;
 #pragma link C++ nestedclass;
 
-//#pragma link C++ class HiggsAnalysis+;
+#endif
+
+#ifdef __CINT__
+
+#pragma link C++ class HiggsAnalysis+;
 
 #endif
+
 #endif
