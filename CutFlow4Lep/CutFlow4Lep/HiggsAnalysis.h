e2b7026fba96346b7b3a0bac541911727862024c
rpetersburg <rpetersburg@gmail.com>
2014-03-27 12:53:28 +0100

    fixing rootcore/cint bug...

--- CutFlow4Lep/CutFlow4Lep/HiggsAnalysis.h
+++ CutFlow4Lep/CutFlow4Lep/HiggsAnalysis.h
@@ -2,11 +2,11 @@
 #define HIGGSANALYSIS_H
 
 #include "D3PDReader/Event.h"
-#include "CutFlow4Lep/ParticleObjects/Jets.h"
-#include "CutFlow4Lep/ParticleObjects/Muon.h"
-#include "CutFlow4Lep/ParticleObjects/Electron.h"
-#include "CutFlow4Lep/ParticleObjects/QuadLepton.h"
-#include "CutFlow4Lep/Output/OutputTree.h"
+//#include "CutFlow4Lep/ParticleObjects/Jets.h"
+//#include "CutFlow4Lep/ParticleObjects/Muon.h"
+//#include "CutFlow4Lep/ParticleObjects/Electron.h"
+//#include "CutFlow4Lep/ParticleObjects/QuadLepton.h"
+//#include "CutFlow4Lep/Output/OutputTree.h"
 
 #include <vector>
 #include <string>
@@ -30,37 +30,37 @@ class HiggsAnalysis
 		
 		void SetOutputFilePath(string newFilePath);	
 
-		Muon *muon;
-		Electron *electron;
-		Electron *electronLoose;
-		Jets *jets;
-		Jets *jetsTruth;
-		Jets *jets_Fid;
-		Jets *jetsTruth_Fid;
+		//Muon *muon;
+		//Electron *electron;
+		//Electron *electronLoose;
+		//Jets *jets;
+		//Jets *jetsTruth;
+		//Jets *jets_Fid;
+		//Jets *jetsTruth_Fid;
 
-		QuadLepton *higgsCandidate4Mu;
-		QuadLepton *higgsCandidate4El;
-		QuadLepton *higgsCandidate2L2L;
+		//QuadLepton *higgsCandidate4Mu;
+		//QuadLepton *higgsCandidate4El;
+		//QuadLepton *higgsCandidate2L2L;
 
-		OutputTree *outputTree;
-		OutputTree *outputTreeCR;
-		OutputTree *outputTreelleeCR;
+		//OutputTree *outputTree;
+		//OutputTree *outputTreeCR;
+		//OutputTree *outputTreelleeCR;
 
-		// Variables that store the final physics objects
-		vector<ChargedLepton *> muEvent;
-		vector<ChargedLepton *> elEvent;
-		vector<ChargedLepton *> elLooseEvent;
-		vector<ChargedLepton *> jetsEvent;
-		vector<ChargedLepton *> jetsTruthEvent;
-		vector<ChargedLepton *> jetsEvent_Fid;
-		vector<ChargedLepton *> jetsTruthEvent_Fid;
+		//// Variables that store the final physics objects
+		//vector<ChargedLepton *> muEvent;
+		//vector<ChargedLepton *> elEvent;
+		//vector<ChargedLepton *> elLooseEvent;
+		//vector<ChargedLepton *> jetsEvent;
+		//vector<ChargedLepton *> jetsTruthEvent;
+		//vector<ChargedLepton *> jetsEvent_Fid;
+		//vector<ChargedLepton *> jetsTruthEvent_Fid;
 	
-		// Physics objects before final overlap removal
-		vector<ChargedLepton *> muOverlap;
-		vector<ChargedLepton *> elOverlap;
-		vector<ChargedLepton *> elLooseOverlap;
-		vector<ChargedLepton *> jetsOverlap;
-		vector<ChargedLepton *> jetsOverlap_Fid;
+		//// Physics objects before final overlap removal
+		//vector<ChargedLepton *> muOverlap;
+		//vector<ChargedLepton *> elOverlap;
+		//vector<ChargedLepton *> elLooseOverlap;
+		//vector<ChargedLepton *> jetsOverlap;
+		//vector<ChargedLepton *> jetsOverlap_Fid;
 
 	
 	private:	
