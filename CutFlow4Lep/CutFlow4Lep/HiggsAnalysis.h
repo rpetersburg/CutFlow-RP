9b253a0a70188ffba035607b9e5e293040b8d7ff
rpetersburg <rpetersburg@gmail.com>
2014-03-27 12:52:23 +0100

    fixing rootcore/cint bug...

--- CutFlow4Lep/CutFlow4Lep/HiggsAnalysis.h
+++ CutFlow4Lep/CutFlow4Lep/HiggsAnalysis.h
@@ -23,56 +23,56 @@ class HiggsAnalysis
 		HiggsAnalysis(TTree *tPhysicsTree);
 		~HiggsAnalysis();
 		
-		//void AnalyzeTree();
-		//void AnalyzeTreeEvent(Long64_t eventNumber);
-		//
-		//void InitializeVar();
-		//
-		//void SetOutputFilePath(string newFilePath);	
+		void AnalyzeTree();
+		void AnalyzeTreeEvent(Long64_t eventNumber);
+		
+		void InitializeVar();
+		
+		void SetOutputFilePath(string newFilePath);	
 
-		//Muon *muon;
-		//Electron *electron;
-		//Electron *electronLoose;
-		//Jets *jets;
-		//Jets *jetsTruth;
-		//Jets *jets_Fid;
-		//Jets *jetsTruth_Fid;
+		Muon *muon;
+		Electron *electron;
+		Electron *electronLoose;
+		Jets *jets;
+		Jets *jetsTruth;
+		Jets *jets_Fid;
+		Jets *jetsTruth_Fid;
 
-		//QuadLepton *higgsCandidate4Mu;
-		//QuadLepton *higgsCandidate4El;
-		//QuadLepton *higgsCandidate2L2L;
+		QuadLepton *higgsCandidate4Mu;
+		QuadLepton *higgsCandidate4El;
+		QuadLepton *higgsCandidate2L2L;
 
-		//OutputTree *outputTree;
-		//OutputTree *outputTreeCR;
-		//OutputTree *outputTreelleeCR;
+		OutputTree *outputTree;
+		OutputTree *outputTreeCR;
+		OutputTree *outputTreelleeCR;
 
-		//// Variables that store the final physics objects
-		//vector<ChargedLepton *> muEvent;
-		//vector<ChargedLepton *> elEvent;
-		//vector<ChargedLepton *> elLooseEvent;
-		//vector<ChargedLepton *> jetsEvent;
-		//vector<ChargedLepton *> jetsTruthEvent;
-		//vector<ChargedLepton *> jetsEvent_Fid;
-		//vector<ChargedLepton *> jetsTruthEvent_Fid;
+		// Variables that store the final physics objects
+		vector<ChargedLepton *> muEvent;
+		vector<ChargedLepton *> elEvent;
+		vector<ChargedLepton *> elLooseEvent;
+		vector<ChargedLepton *> jetsEvent;
+		vector<ChargedLepton *> jetsTruthEvent;
+		vector<ChargedLepton *> jetsEvent_Fid;
+		vector<ChargedLepton *> jetsTruthEvent_Fid;
 	
-		//// Physics objects before final overlap removal
-		//vector<ChargedLepton *> muOverlap;
-		//vector<ChargedLepton *> elOverlap;
-		//vector<ChargedLepton *> elLooseOverlap;
-		//vector<ChargedLepton *> jetsOverlap;
-		//vector<ChargedLepton *> jetsOverlap_Fid;
+		// Physics objects before final overlap removal
+		vector<ChargedLepton *> muOverlap;
+		vector<ChargedLepton *> elOverlap;
+		vector<ChargedLepton *> elLooseOverlap;
+		vector<ChargedLepton *> jetsOverlap;
+		vector<ChargedLepton *> jetsOverlap_Fid;
 
 	
-	//private:	
-	//	TTree *m_physicsTree;
-	//	
-	//	D3PDReader::Event *m_event;
-	//	
-	//	TString m_outputFilePath;
+	private:	
+		TTree *m_physicsTree;
+		
+		D3PDReader::Event *m_event;
+		
+		TString m_outputFilePath;
 
-	//	TString m_currFileName;
+		TString m_currFileName;
 
-	//	TString outputFilePath;
+		TString outputFilePath;
 
 };
 
