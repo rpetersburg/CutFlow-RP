52b3198b75584ee8b9db57bd1b8a9b917bed0292
rpetersburg <rpetersburg@gmail.com>
2014-03-27 13:11:07 +0100

    fixing rootcore/cint bug...

--- CutFlow4Lep/CutFlow4Lep/ParticleObjects/QuadLepton.h
+++ CutFlow4Lep/CutFlow4Lep/ParticleObjects/QuadLepton.h
@@ -4,27 +4,27 @@
 //#include <TTree.h>
 //#include <TString.h>
 //#include <TLorentzVector.h>
-//
-//#include "DiLepton.h"
+
+#include "DiLepton.h"
 
 using namespace std;
 
 class QuadLepton
 {
-	//public:
-	//	QuadLepton();
-	//	~QuadLepton();
+	public:
+		QuadLepton();
+		~QuadLepton();
 
-	//	//DiLepton getDiLepton1();
-	//	//DiLepton getDiLepton2();
-
-
-	//protected:
-	//	DiLepton diLepton1;
-	//	DiLepton diLepton2;
+		DiLepton getDiLepton1();
+		DiLepton getDiLepton2();
 
 
-	//private:
+	protected:
+		DiLepton diLepton1;
+		DiLepton diLepton2;
+
+
+	private:
 
 
 };
