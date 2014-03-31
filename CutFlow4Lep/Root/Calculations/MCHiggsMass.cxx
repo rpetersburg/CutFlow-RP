85a39851e6c9e84379de7c9fdf0fafa73561fc42
rpetersburg <rpetersburg@gmail.com>
2014-03-31 14:17:42 +0200

    changing file types

---
+++ CutFlow4Lep/Root/Calculations/MCHiggsMass.cxx
@@ -0,0 +1,19 @@
+#include "CutFlow4Lep/Calculations/MCHiggsMass.h"
+
+
+MCHiggsMass::MCHiggsMass(D3PDReader::Event *tEvent, TString tFileName) : m_event(tEvent), m_fileName(tFileName)
+{
+
+
+}
+
+MCHiggsMass::~MCHiggsMass()
+{
+
+
+}
+
+Double_t MCHiggsMass::getMass()
+{
+
+}
